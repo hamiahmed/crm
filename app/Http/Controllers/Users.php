35caf32505a3d23604f490731c39f5a7f42406b0
hamiiahmed <hamad223.hk@gmail.com>
2024-03-21 03:06:45 +0500

    no comment

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -12,7 +12,6 @@ class Users extends Controller
 {
     public function login(Request $req)
     {
-
         $validator = Validator::make($req->all(), [
             'email_username' => 'required',
             'password'       => 'required',
@@ -25,14 +24,12 @@ class Users extends Controller
                 ->where('username', $req->input('username'))
                 ->first();
 
-               
-
             if (!empty($user)) {
 
                 // $hashcheck = Hash::check($req->input('password'), $user->password);
 
                 // if (!empty($hashcheck)) {
-                    
+
 
                     $user_array = [
                         'user_id'   => $user['user_id'],
@@ -42,18 +39,17 @@ class Users extends Controller
                         'role_name' => $user['role']
                     ];
 
-                    
-                    $module_permissions = Permission::select('permissions.*', 'modules.module_name')->join('modules', 'modules.id', '=', 'permissions.module_id')->where('role_id', $user['role_id'])->get();
 
-                    foreach ($module_permissions as $module_permission) {
-                        $permissions[$module_permission['module_name']] = json_decode($module_permission['details'], true);
-                    }
-                    
+                    // $module_permissions = Permission::select('permissions.*', 'modules.module_name')->join('modules', 'modules.id', '=', 'permissions.module_id')->where('role_id', $user['role_id'])->get();
+
+                    // foreach ($module_permissions as $module_permission) {
+                    //     $permissions[$module_permission['module_name']] = json_decode($module_permission['details'], true);
+                    // }
+
                     session()->put('userdata', $user_array);
-                    session()->put('permissions', $permissions);
+                    // session()->put('permissions', $permissions);
 
                     return redirect('/');
-
                 // } else {
                 //     return redirect('login')->with('error', 'Username or Password is incorrect!');
                 // }
--- routes/web.php
+++ routes/web.php
@@ -20,79 +20,7 @@ Route::middleware([user_authentication::class])->group(function () {
 
         Route::group(['prefix' => 'dashboard'], function () {
             Route::get('/', [Home::class, 'dashboard']);
-        });
-
-
-        Route::group(['prefix' => 'qualification'], function () {
-            Route::get('/', [Home::class, 'qualification']);
-            Route::post('/create', [Home::class, 'add_qualification']);
-            Route::post('/update', [Home::class, 'update_qualification']);
-            Route::get('/single', [Home::class, 'get_single_qualification']);
-        });
-
-        Route::group(['prefix' => 'transfer'], function () {
-            Route::get('/', [Home::class, 'transfer']);
-            Route::post('/create', [Home::class, 'add_transfer']);
-            Route::post('/update', [Home::class, 'update_transfer']);
-            Route::get('/single', [Home::class, 'get_single_transfer']);
-        });
-
-        Route::group(['prefix' => 'acr'], function () {
-            Route::get('/', [Home::class, 'acr']);
-            Route::post('/create', [Home::class, 'add_acr']);
-            Route::post('/update', [Home::class, 'update_acr']);
-            Route::get('/single', [Home::class, 'get_single_acr']);
-        });
-
-        Route::group(['prefix' => 'leave'], function () {
-            Route::get('/', [Home::class, 'leave']);
-            Route::post('/create', [Home::class, 'add_leave']);
-            Route::post('/update', [Home::class, 'update_leave']);
-            Route::get('/single', [Home::class, 'get_single_leave']);
-        });
-
-
-        Route::group(['prefix' => 'training'], function () {
-            Route::get('/', [Home::class, 'training']);
-            Route::post('/create', [Home::class, 'add_training']);
-            Route::post('/update', [Home::class, 'update_training']);
-            Route::get('/single', [Home::class, 'get_single_training']);
-        });
-
-        Route::group(['prefix' => 'promotion'], function () {
-            Route::get('/', [Home::class, 'promotion']);
-            Route::post('/create', [Home::class, 'add_promotion']);
-            Route::post('/update', [Home::class, 'update_promotion']);
-            Route::get('/single', [Home::class, 'get_single_promotion']);
-        });
-
-        Route::group(['prefix' => 'service'], function () {
-            Route::get('/', [Home::class, 'service']);
-            Route::post('/create', [Home::class, 'add_service']);
-            Route::post('/update', [Home::class, 'update_service']);
-            Route::get('/single', [Home::class, 'get_single_service']);
-        });
-
-        Route::group(['prefix' => 'appointment'], function () {
-            Route::get('/', [Home::class, 'appointment']);
-            Route::post('/create', [Home::class, 'add_appointment']);
-            Route::post('/update', [Home::class, 'update_appointment']);
-            Route::get('/single', [Home::class, 'get_single_appointment']);
-        });
-
-        Route::group(['prefix' => 'family'], function () {
-            Route::get('/', [Home::class, 'family']);
-            Route::post('/create_spouce', [Home::class, 'add_spouce']);
-            Route::post('/create_marital', [Home::class, 'add_martial']);
-            Route::post('/update_spouce', [Home::class, 'update_spouce']);
-            Route::get('/single_spouce',  [Home::class, 'get_single_spouce']);
-            Route::post('/update_marital', [Home::class, 'update_marital']);
-            Route::get('/single_marital', [Home::class, 'get_single_marital']);
-        });
-
-       
-           
-           
+        });   
 
         // Route::group(['prefix' => 'roles'], function () {
         //     Route::get('/', [Roles::class, 'index']);
@@ -107,7 +35,7 @@ Route::middleware([user_authentication::class])->group(function () {
     });
 
 
-    Route::get('/', [Home::class, 'information']);
+    // Route::get('/', [Home::class, 'information']);
     Route::post('/global_delete', [Home::class, 'global_delete']);
     Route::get('/admin_signup', [Home::class, 'admin_signup']);
     Route::post('/admin_signup', [Home::class, 'create_admin']);
@@ -124,8 +52,8 @@ Route::middleware([logincheck::class])->group(function () {
     });
 });
 
-// Route::get('/', function () {
-//     return view('index');
-// });
+Route::get('/', function () {
+    return view('layouts.master');
+});
 
 Route::POST('/login', [Users::class, 'login']);
